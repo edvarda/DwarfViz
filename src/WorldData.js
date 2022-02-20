fda84c79f42948eb60cc0cd83fb12723c3eac2d2
Love Wessman <lwessman@kth.se>
2022-02-20 18:13:42 +0100

    Add first trial of TreeMap
    Shows art style data in the following hierarchy:
    ArtStyles{Dance/Poetry/Music}
    ---/Forms(e.g. Specific types of Dance)
    
    The size of the form is dictated by the amount of occurences of that artStyle in written_contents from the DF storyteller API.

--- src/WorldData.js
+++ src/WorldData.js
@@ -14,6 +14,11 @@ export const useWorldData = () => {
         regionsGeoJSON: await import('./data/regions.geo.json'),
         regions: await import('./data/regions.json'),
         mapImageURL: await (await import('./data/image.png')).default,
+        writtenContents:  await ( await import('./data/written_contents.json')).data,
+        danceForms:  await (await import('./data/dance_forms.json')).data,
+        poeticForms:  await (await import('./data/poetic_forms.json')).data,
+        musicalForms:  await (await import('./data/musical_forms.json')).data,
+
       };
     };
     const fetchRemoteData = async () => {
@@ -22,6 +27,11 @@ export const useWorldData = () => {
           regionsGeoJSON: await import('./data/regions.geo.json'),
           regions: (await axios.get(`${storytellerURL}/regions`)).data,
           mapImageURL: `${storytellerURL}/map_images/0/image.png`,
+          // TODO: Make written contents actually get all available, not just first 500
+          writtenContents: (await axios.get(`${storytellerURL}/written_contents?per_page=500`)).data,
+          danceForms: (await axios.get(`${storytellerURL}/written_contents?per_page=500`)).data,
+          poeticForms: (await axios.get(`${storytellerURL}/written_contents?per_page=500`)).data,
+          musicalForms: (await axios.get(`${storytellerURL}/written_contents?per_page=500`)).data,
         };
       } catch (error) {
         setIsError(true);
