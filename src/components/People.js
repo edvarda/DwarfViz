65ff3642e4afee16e58a6d3cb402156581be473e
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2022-02-25 23:21:02 +0100

    add copypasta of mike bostock's zoomable sunburst

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,12 @@
 import { CivPopulation } from './CivPopulation/CivPopulation.js';
-
+import Sunburst from './Sunburst.js';
 const People = ({ entityPopulation }) => {
-  return <CivPopulation entityPopulation={entityPopulation} width={450} height={250} />;
+  return (
+    <>
+      <CivPopulation entityPopulation={entityPopulation} width={450} height={250} />
+      <Sunburst />
+    </>
+  );
 };
 
 export default People;
