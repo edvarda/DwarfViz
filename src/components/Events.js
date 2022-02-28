3acc7d793bf5a69a59fd7ce5b8c67595ec372974
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2022-02-28 21:33:19 +0100

    move timeline to events component

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,18 @@
+import BrushableTimeline from './BrushableTimeline/BrushableTimeline.js';
 const Events = ({ data }) => {
-  return <>Events</>;
+  return (
+    <svg width='412' height='412'>
+      <g>
+        <BrushableTimeline
+          data={data}
+          width={412}
+          height={360}
+          //setBrushExtent={setBrushExtent}
+          //xValue={xValue}
+        />
+      </g>
+    </svg>
+  );
 };
 
 export default Events;
