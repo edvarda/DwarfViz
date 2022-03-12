0eaf0e16dcc4316a26b0f9f8ef0d8882619441a7
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2022-03-12 09:40:06 +0100

    linting

--- src/components/EntityDetails/CivDetails.js
+++ src/components/EntityDetails/CivDetails.js
@@ -12,17 +12,17 @@ const CivDetails = () => {
   else {
     let evalEntity = selectedEntity;
     while (parentCiv == undefined) {
-      if(evalEntity.entity_link.length == 0) break;
+      if (evalEntity.entity_link.length == 0) break;
       for (const link of evalEntity.entity_link) {
-        if(link.type == 'PARENT') {
+        if (link.type == 'PARENT') {
           const parent = entities.find((ent) => ent.id == link.target);
-          if(parent.type == 'civilization') parentCiv = parent;
+          if (parent.type == 'civilization') parentCiv = parent;
           else evalEntity = parent;
         }
       }
     }
   }
-  if(parentCiv != undefined) {
+  if (parentCiv != undefined) {
     const entPop = entityPopulations.find((e) => e.civ_id == parentCiv.id);
     let text =
       entPop != undefined ? (
@@ -34,15 +34,17 @@ const CivDetails = () => {
       ) : (
         <p>Race: {parentCiv.race}</p>
       );
+
     return (
-      <div style={{border:"1px solid lightGrey",padding:'15px',borderRadius:'5px'}}>
-        <h2 style={{fontSize:'1.3em'}}>{parentCiv.name}</h2>
-        <h3 className='mb-2 text-muted' style={{fontSize:'1.15em'}}>Type: {parentCiv.type} </h3>
+      <div style={{ border: '1px solid lightGrey', padding: '15px', borderRadius: '5px' }}>
+        <h2 style={{ fontSize: '1.3em' }}>{parentCiv.name}</h2>
+        <h3 className='mb-2 text-muted' style={{ fontSize: '1.15em' }}>
+          Type: {parentCiv.type}{' '}
+        </h3>
         {text}
       </div>
     );
-  }
-  else return null;
+  } else return null;
 };
 
 export default CivDetails;
