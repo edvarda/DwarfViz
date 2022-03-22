0ce1c83f5dc3332799216c8f6b968e6b4514470b
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2022-03-22 14:22:45 +0100

    update backend url with new domain

--- src/dwarfviz.config.js
+++ src/dwarfviz.config.js
@@ -1,5 +1,5 @@
 const config = {
   useStaticData: true, // set to true to only load static data files
-  storytellerURL: 'http://178.62.223.248/api', // address for dwarf storyteller apiserver
+  storytellerURL: 'https://api.dwarfviz.com/api', // address for dwarf storyteller apiserver
 };
 export default config;
